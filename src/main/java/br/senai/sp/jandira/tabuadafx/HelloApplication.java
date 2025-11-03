a0bd70f553bac8a00509298331850de268e78e75
JEAN COSTA ALVES DA SILVA <25203640@CFP127ALUNO.LOCAL>
2025-11-03 11:26:49 -0300

    Iniciando stage

--- src/main/java/br/senai/sp/jandira/tabuadafx/HelloApplication.java
+++ src/main/java/br/senai/sp/jandira/tabuadafx/HelloApplication.java
@@ -1,8 +1,11 @@
 package br.senai.sp.jandira.tabuadafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +13,46 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        //Definir tamanho da tela (stage)
+        stage.setWidth(600);
+        stage.setHeight(700);
+
+        //Componente principal
+        VBox root = new VBox();
+        Scene scene = new Scene(root);
+
+        //Criando Header
+        VBox header = new VBox();
+        header.setStyle("-fx-padding: 10; -fx-background-color: #027fff");
+        root.getChildren().addAll(header);
+
+
+        Label labelTitulo = new Label("Tabuada");
+        labelTitulo.setStyle("-fx-text-fill: #fafafa; -fx-font-size: 24px; -fx-font-weight: Bold ");
+        Label labelSubtitulo = new Label("Construa tabudas sem limites!");
+        labelSubtitulo.setStyle("-fx-text-fill: #fafafa; -fx-font-size: 14px;");
+
+        header.getChildren().add(labelTitulo);
+        header.getChildren().add(labelSubtitulo);
+
+        //Criar Multiplicando
+        HBox multiplicandoBox = new HBox();
+        multiplicandoBox.setStyle("-fx-padding: 10");
+        Label labelMultiplicando = new Label("Multiplicando");
+        labelMultiplicando.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px;");
+        TextField textFieldfMultiplicando = new TextField();
+
+        multiplicandoBox.getChildren().add(labelMultiplicando);
+        multiplicandoBox.getChildren().add(textFieldfMultiplicando);
+        root.getChildren().add(multiplicandoBox);
+
+
         stage.setScene(scene);
+        stage.setTitle("Tabuada");
         stage.show();
+
+
+
     }
 }
