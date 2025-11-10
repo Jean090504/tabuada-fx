18608eb8f6598278089df9e084524501e5321bb8
JEAN COSTA ALVES DA SILVA <25203640@CFP127ALUNO.LOCAL>
2025-11-10 10:59:27 -0300

    Adicionando função calcularTabuada

--- src/main/java/br/senai/sp/jandira/tabuadafx/HelloApplication.java
+++ src/main/java/br/senai/sp/jandira/tabuadafx/HelloApplication.java
@@ -2,9 +2,7 @@ package br.senai.sp.jandira.tabuadafx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -12,6 +10,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    TextField textFieldfMultiplicando;
+    TextField textFieldfMenorMultiplicador;
+    TextField textFieldfMaiorMultiplicador;
+    ListView listaTabuada;
+
     @Override
     public void start(Stage stage) throws IOException {
 
@@ -47,14 +50,14 @@ public class HelloApplication extends Application {
         gridFormulario.setStyle("-fx-padding: 10px; -fx-background-color: #add5fa;");
         Label labelMultiplicando = new Label("Multiplicando");
         labelMultiplicando.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px; -fx-label-padding: 10px");
-        TextField textFieldfMultiplicando = new TextField();
+        textFieldfMultiplicando = new TextField();
 
         Label labelMenorMultiplicador = new Label("Menor Multiplicador");
-        TextField textFieldfMenorMultiplicador = new TextField();
+        textFieldfMenorMultiplicador = new TextField();
         labelMenorMultiplicador.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px; -fx-label-padding: 10px");
 
         Label labelMaiorMultiplicador = new Label("Maior Multiplicador");
-        TextField textFieldfMaiorMultiplicador = new TextField();
+        textFieldfMaiorMultiplicador = new TextField();
         labelMaiorMultiplicador.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px; -fx-label-padding: 10px");
 
         gridFormulario.add(labelMultiplicando, 0, 0);
@@ -66,27 +69,68 @@ public class HelloApplication extends Application {
 
         //Criar o componente de botões
         HBox boxBotoes = new HBox();
+
         Button btnCalcular = new Button("Calcular");
         btnCalcular.setStyle("");
+        btnCalcular.setOnAction(e -> {
+            calcularTabuada();
+        });
 
         Button btnLimpar = new Button("Limpar");
         btnLimpar.setStyle("");
+        btnLimpar.setOnAction(e -> {
+
+        });
 
         Button btnSair = new Button("Sair");
         btnSair.setStyle("");
+        btnSair.setOnAction(e -> {
+
+        });
 
         //Adicionar os botões ba boxBotões
         boxBotoes.getChildren().addAll(btnCalcular, btnLimpar, btnSair);
 
+        //Adicionando Lista
+        VBox boxResultado = new VBox();
+        Label labelResultado = new Label("Resultados:");
+        labelResultado.setStyle("-fx-text-fill: #1c0808; -fx-font-size: 18px; -fx-font-weight: bold");
+
+        //Adicionar o listView
+        listaTabuada = new ListView();
+
+        //Adiciona o Label ao boxResultado
+        boxResultado.getChildren().add(labelResultado);
 
         //adicionar componentes ao root
         root.getChildren().addAll(header);
         root.getChildren().add(gridFormulario);
         root.getChildren().addAll(boxBotoes);
+        root.getChildren().add(boxResultado);
+        root.getChildren().addAll(listaTabuada);
 
+    }
 
+    public void calcularTabuada() {
+        int multiplicando = Integer.parseInt(textFieldfMultiplicando.getText());
+        int menorMultiplicador = Integer.parseInt(textFieldfMenorMultiplicador.getText());
+        int maiorMultiplicador = Integer.parseInt(textFieldfMaiorMultiplicador.getText());
 
+        int tamanho = (maiorMultiplicador - menorMultiplicador) + 1;
 
+        String[] tabuada = new String[tamanho];
+
+        int contador = 0;
+        while (contador < tabuada.length) {
+            int produto = multiplicando * menorMultiplicador;
+
+            tabuada[contador] = multiplicando + " x " + menorMultiplicador + " = " + produto;
+
+            contador++;
+            menorMultiplicador++;
+        }
+
+        listaTabuada.getItems().addAll(tabuada);
 
     }
 }
