54f4a55a3ba50e998c207aef40c7f05974b03e95
JEAN COSTA ALVES DA SILVA <25203640@CFP127ALUNO.LOCAL>
2025-11-10 08:51:41 -0300

    Adicionando grid e botoes

--- src/main/java/br/senai/sp/jandira/tabuadafx/HelloApplication.java
+++ src/main/java/br/senai/sp/jandira/tabuadafx/HelloApplication.java
@@ -2,12 +2,13 @@ package br.senai.sp.jandira.tabuadafx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -21,36 +22,69 @@ public class HelloApplication extends Application {
         //Componente principal
         VBox root = new VBox();
         Scene scene = new Scene(root);
+        stage.setTitle("Tabuada");
+
+        stage.setScene(scene);
+        stage.show();
+
 
         //Criando Header
         VBox header = new VBox();
-        header.setStyle("-fx-padding: 10; -fx-background-color: #027fff");
-        root.getChildren().addAll(header);
+        header.setStyle("-fx-padding: 10; -fx-background-color: #2d5d9a");
+
 
 
         Label labelTitulo = new Label("Tabuada");
         labelTitulo.setStyle("-fx-text-fill: #fafafa; -fx-font-size: 24px; -fx-font-weight: Bold ");
-        Label labelSubtitulo = new Label("Construa tabudas sem limites!");
+        Label labelSubtitulo = new Label("Construa tabuadas sem limites!");
         labelSubtitulo.setStyle("-fx-text-fill: #fafafa; -fx-font-size: 14px;");
 
         header.getChildren().add(labelTitulo);
         header.getChildren().add(labelSubtitulo);
 
         //Criar Multiplicando
-        HBox multiplicandoBox = new HBox();
-        multiplicandoBox.setStyle("-fx-padding: 10");
+        GridPane gridFormulario = new GridPane();
+        gridFormulario.setStyle("-fx-padding: 10px; -fx-background-color: #add5fa;");
         Label labelMultiplicando = new Label("Multiplicando");
-        labelMultiplicando.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px;");
+        labelMultiplicando.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px; -fx-label-padding: 10px");
         TextField textFieldfMultiplicando = new TextField();
 
-        multiplicandoBox.getChildren().add(labelMultiplicando);
-        multiplicandoBox.getChildren().add(textFieldfMultiplicando);
-        root.getChildren().add(multiplicandoBox);
+        Label labelMenorMultiplicador = new Label("Menor Multiplicador");
+        TextField textFieldfMenorMultiplicador = new TextField();
+        labelMenorMultiplicador.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px; -fx-label-padding: 10px");
+
+        Label labelMaiorMultiplicador = new Label("Maior Multiplicador");
+        TextField textFieldfMaiorMultiplicador = new TextField();
+        labelMaiorMultiplicador.setStyle("-fx-text-fill: #000000; -fx-font-size: 14px; -fx-label-padding: 10px");
+
+        gridFormulario.add(labelMultiplicando, 0, 0);
+        gridFormulario.add(textFieldfMultiplicando, 1, 0);
+        gridFormulario.add(labelMenorMultiplicador, 0, 1);
+        gridFormulario.add(textFieldfMenorMultiplicador, 1, 1);
+        gridFormulario.add(labelMaiorMultiplicador, 0, 2);
+        gridFormulario.add(textFieldfMaiorMultiplicador, 1, 2);
+
+        //Criar o componente de botões
+        HBox boxBotoes = new HBox();
+        Button btnCalcular = new Button("Calcular");
+        btnCalcular.setStyle("");
+
+        Button btnLimpar = new Button("Limpar");
+        btnLimpar.setStyle("");
+
+        Button btnSair = new Button("Sair");
+        btnSair.setStyle("");
+
+        //Adicionar os botões ba boxBotões
+        boxBotoes.getChildren().addAll(btnCalcular, btnLimpar, btnSair);
 
 
-        stage.setScene(scene);
-        stage.setTitle("Tabuada");
-        stage.show();
+        //adicionar componentes ao root
+        root.getChildren().addAll(header);
+        root.getChildren().add(gridFormulario);
+        root.getChildren().addAll(boxBotoes);
+
+
 
 
 
